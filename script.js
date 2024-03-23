437d980751d323382edbe26a190164b0dfd125a5
Hukako <hukako2004@gmail.com>
2024-03-23 01:36:39 -0300

    Web dev v0.4.3

--- script.js
+++ script.js
@@ -2,11 +2,23 @@ const MAIN_CONTAINER = document.getElementById('main_container')
 const BUTTON_SOBRE_MIM = document.getElementById('sobre_mim')
 const BUTTON_CONHECIMENTOS = document.getElementById('conhecimentos')
 const BUTTON_PROJETOS = document.getElementById('projetos')
+let a = document.createElement("a")
+if(window.clientWidth < 576) {
+    BUTTON_SOBRE_MIM.outerHTML = '<a id="sobre_mim" href="#conteudo_sobre_mim">Sobre mim</a>'
+}
 
+/////
 function moverCarousel() {
     let carrousel_value = 0
+    let client_width = setInterval(width_timer, 500)
+    function width_timer(value) {
+        value = document.body.clientWidth
+        return value
+    }
+    console.log(client_width)
     
     //  movimento automático, valor padrão 8000ms
+    console.log(document.body.clientWidth)
     if(document.body.clientWidth > 576) {
         setInterval(() => {
             carrousel_value += 100
@@ -32,7 +44,7 @@ function moverCarousel() {
         MAIN_CONTAINER.style.right = carrousel_value + "%"
     })
 }
-//moverCarousel()
+moverCarousel()
 
 
 setInterval(() => {
