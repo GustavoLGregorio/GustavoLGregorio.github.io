2049de7259a87c61d7a8933492c31766b1926feb
Gustavo Luiz Gregorio <gustavo.l.gregorio@gmail.com>
2025-10-26 10:53:26 -0300

    Update carousel

--- src/components/TechCarousel.tsx
+++ src/components/TechCarousel.tsx
@@ -17,18 +17,18 @@ import iconMongodb from "./../assets/icons/mongodb.svg";
 
 import iconMysql from "./../assets/icons/mysql.svg";
 
-type TechonlogyType = {
+type TechnologyType = {
     id: number;
     name: string;
     icon: string;
 };
 
-export default function Technologies() {
+export default function TechCarousel() {
     const containerRef = useRef<HTMLDivElement>(null);
-    const carrousselRef = useRef<HTMLDivElement>(null);
-    const [carrousselPos, setCarrousselPos] = useState<number>(0);
+    const carouselRef = useRef<HTMLDivElement>(null);
+    const [carouselPos, setCarouselPos] = useState<number>(0);
 
-    const techs: TechonlogyType[] = [
+    const techs: TechnologyType[] = [
         {
             id: 0,
             name: "html",
@@ -96,56 +96,53 @@ export default function Technologies() {
         },
     ];
 
+    const duplicatedTechs = [...techs, ...techs, ...techs];
+
     useEffect(() => {
         const loopId = setInterval(() => {
-            setCarrousselPos((c) => c - 1);
+            setCarouselPos((c) => c - 2);
         }, 16);
 
         return () => clearInterval(loopId);
     }, []);
 
     useEffect(() => {
-        if (
-            carrousselPos <=
-            -(
-                carrousselRef.current!.scrollWidth -
-                containerRef.current!.clientWidth
-            )
-        ) {
-            setCarrousselPos(-1);
+        if (!carouselRef.current || !containerRef.current) return;
+
+        const itemWidth = 96;
+        const oneSetWidth = techs.length * itemWidth;
+
+        if (Math.abs(carouselPos) >= oneSetWidth) {
+            setCarouselPos(0);
+            carouselRef.current.style.left = "0px";
+        } else {
+            carouselRef.current.style.left = `${carouselPos}px`;
         }
-        if (carrousselRef.current) {
-            carrousselRef.current.style.left = `${carrousselPos}px`;
-        }
-    }, [carrousselPos]);
+    }, [carouselPos, techs.length]);
 
     return (
         <div
             ref={containerRef}
             // prettier-ignore
             className="relative w-full overflow-x-hidden py-2
-            before:absolute before:top-0 before:left-0 before:z-10
-            before:h-full before:w-16 before:bg-gradient-to-l
-            before:from-transparent before:to-background
-            after:absolute after:top-0 after:right-0 after:h-full
-            after:w-16 after:bg-gradient-to-r
-            after:from-transparent after:to-background"
+                before:absolute before:top-0 before:left-0 before:z-10
+                before:h-full before:w-16 before:bg-gradient-to-l
+                before:from-transparent before:to-background
+                after:absolute after:top-0 after:right-0 after:h-full
+                after:w-16 after:bg-gradient-to-r
+                after:from-transparent after:to-background"
         >
-            <div
-                className="relative flex w-[300%] flex-row gap-x-4"
-                ref={carrousselRef}
-            >
-                {techs.map((tech) => (
+            <div className="relative flex flex-row gap-x-4" ref={carouselRef}>
+                {duplicatedTechs.map((tech, index) => (
                     <div
                         className="flex flex-col gap-2"
-                        key={tech.id}
+                        key={`${tech.id}-${index}`}
                     >
                         <img
                             src={tech.icon}
-                            className="aspect-square w-20 max-w-32 min-w-20"
-                            alt=""
+                            className="bg-background flex aspect-square w-20 max-w-32 min-w-20 items-center justify-center rounded-lg text-4xl"
                         />
-                        <h4 className="cs-font-cascadia-code text-center font-bold uppercase">
+                        <h4 className="cs-font-cascadia-code text-center text-sm font-bold uppercase">
                             {tech.name}
                         </h4>
                     </div>
